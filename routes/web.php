9a1584d3d24b41dedde46b81f50eaac2f737738a
carlos <carlosasjr2003@hotmail.com>
2022-03-03 12:32:23 -0300

    model message and route auth for chat

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(ChatController::class)
     ->prefix('chat')
     ->name('chat.')
+    ->middleware('auth')
     ->group(function () {
         Route::get('/', 'index')->name('index');
     });
